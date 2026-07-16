278d54d9732461272e163b2bec56df81ec811450
RenanTheFato <renan.santana007@hotmail.com>
2026-07-16 14:19:32 -0300

    instance application main

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+"""Application Entry Point
+
+Initialize MainWindow and his inherited widgets
+"""
+
+import sys
+
+from PySide6.QtWidgets import QApplication
+from pybit.unified_trading import HTTP
+
+from ui.main_window import MainWindow
+
 def main():
-    print("Hello from smart-finance!")
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
