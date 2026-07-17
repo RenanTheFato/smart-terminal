303bf087a9f7eebea050d0145bf743adb4c18c3c
RenanTheFato <renan.santana007@hotmail.com>
2026-07-17 18:51:53 -0300

    add method to pull kline data from api and manipulate it to display on widget

--- api/connector.py
+++ api/connector.py
@@ -4,6 +4,7 @@ import threading
 from pybit.unified_trading import HTTP
 from pybit.unified_trading import WebSocket
 from loguru import logger
+from datetime import datetime, timezone
 
 class ByBitClient:
   def __init__(self, testnet: bool = False):
@@ -28,9 +29,33 @@ class ByBitClient:
       logger.error(f"Error on try to connect into ByBit on REST: {e}")
       return False
     
+  # REST Get for Symbol Kline Data
+  def get_kline_data(self, symbol: str = "BTCUSDT", interval: str = "3"):
+    try:
+      kline_data = self._session.get_kline(category="linear", symbol=symbol, interval=interval)
+      raw_candles = kline_data["result"]["list"]
+
+      # Reverse Candles Data
+      candles = [
+        (
+          datetime.fromtimestamp(float(c[0]) / 1000, tz=timezone.utc), # -> Timestamp
+          float(c[1]), # -> Open
+          float(c[2]), # -> High
+          float(c[3]), # -> Low
+          float(c[4]) # -> Close
+        )
+        for c in reversed(raw_candles)
+      ]
+
+      logger.info(f"Kline Data Successfully Fetched. Symbol: {symbol}")
+      return candles
+    except Exception as e:
+      logger.error(f"Error on try to fetch kline data from symbol {symbol}; Error: {e}")
+      return None
+    
 
   # Init Ticker Stream WS
-  def start_ticker_strem(self, symbol: str | str = "BTCUSDT"):
+  def start_ticker_strem(self, symbol: str = "BTCUSDT"):
     self._symbol = symbol
     self._should_run = True
     self._connect_ws()
